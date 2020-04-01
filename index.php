d10a3dee4c627f610047683c7d58d9980ffbdf64
Aamandeepkaur <60448754+Aamandeepkaur@users.noreply.github.com>
2020-03-31 20:18:23 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>The coffee house</title>
+<link rel="stylesheet"  href="javajam.css" media="print">
+</head>
+<body>
+<div id="wrapper">
+<header>
+    <h1>JavaJam Coffee House</h1>
+    
+    
+</header>
+<nav>
+<a href="index.html">Home</a> &nbsp; 
+<a href="menu.html">Menu</a> &nbsp; 
+<a href="music.html">Music</a> &nbsp; 
+<a href="jobs.html">Jobs</a>
+</nav>
+<main>
+
+Follow the Winding Road to JavaJam
+We&#39;re a little out of the way, but take a drive down Route 42 to JavaJam today! Indulge in our locally roasted free-trade coffee and home-made pastries. You&#39;ll feel right at home at JavaJam!
+JavaJam Coffee House features:
+<ul>
+  <li>Specialty Coffee and Tea</li>
+  <li>Bagels, Muffins, and Organic Snacks</li>
+  <li>Music and Poetry Readings</li>
+  <li>Open Mic Night</li>
+</ul>
+<div>
+54321 Route 42<br>
+Ellison Bay, WI 54210<br>
+888-555-5555<br><br>
+</div> 
+</main>
+<footer>
+Copyright &copy; 2016 JavaJam Coffee House<br>
+<a href="mailtoAmandeep@kaur.com">Amandeep@Kaur.com</a>
+</footer>
+</div>
+</body>
+</html>
